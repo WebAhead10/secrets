cd9611eb9548235d28a10bb3685e05277612f1c3
EliasSalom <elias.29@hotmail.com>
2021-07-27 22:24:10 +0300

    add a new page and readme file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from "react";
+import Login from "./Login";
 
-
-function App() {
-  return null
+function App() { //<<< from here the app will start to read
+  return (
+  <Login> 
+    {/* the Login tag is just a file that i'm import up to show */}
+  </Login>
+  )  
 }
 
 export default App;
